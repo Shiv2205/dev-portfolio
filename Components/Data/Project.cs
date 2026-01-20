626b53a30365e9b35e03b85b4c275e770c94e111
Shivam Veerabudren <shivamveerabudren@gmail.com>
2026-01-20 08:29:52 -0500

    Defined Project obj to use in Projects page

---
+++ Components/Data/Project.cs
@@ -0,0 +1,12 @@
+namespace blzrTest.Components.Data
+{
+    public class Project
+    {
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public List<string>? Technologies { get; set; }
+        public string? ImageUrl { get; set; }
+        public string? RepoUrl { get; set; }
+        public string? LiveUrl { get; set; }
+    }
+}
